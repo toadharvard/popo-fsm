a3f122128fa0a4b399340617c0d00c30da40c3b9
Nitish Rathi <nitish@redpanda.co.in>
2015-03-25 15:22:40 +0530

    add description

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 
+try:
+    long_description = open('README.md').read()
+except IOError:
+    long_description = ''
+
 setup(name='popo-fsm',
       version='0.0.1',
+      description='Plain old python object finite state machine support.',
+      long_description=long_description,
       url='https://github.com/redpandalabs/popo-fsm',
       py_modules=['popo_fsm'],
       license='MIT License',
